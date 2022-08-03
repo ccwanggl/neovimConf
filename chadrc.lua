d499b9e64f9fa1fb37b5a00edddb126eda153fde
guoliang <tswanggl@gmail.com>
2022-08-03 09:34:01 +0800

    First commit

---
+++ chadrc.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+M.ui = {
+   theme = "chadtain",
+}
+
+M.mappings = require "custom.mappings"
+
+return M
---
+++ mappings.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+
+M.general = {
+  i = {
+    ["jk"] = { "<ESC>", "Escape insert mode", opts = { nowait = true }},
+  }
+}
+
+return M
