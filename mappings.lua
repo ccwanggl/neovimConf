17888e165c85d8ac10619c663745fa97900128b2
guoliang <tswanggl@gmail.com>
2023-02-11 13:27:56 +0800

    change buffer switch use ','

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,15 @@ M.general = {
   i = {
     ["jk"] = { "<ESC>", "Escape insert mode", opts = { nowait = true } },
   },
+
+  n = {
+    [","] = {
+      function()
+        require("nvchad_ui.tabufline").tabuflineNext()
+      end,
+      "goto next buffer",
+    },
+  },
 }
 
 return M
