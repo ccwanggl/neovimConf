9760900833e24639c0c6cb82604dd9a69ed46a89
Guoliang WANG <tswanggl@gmail.com>
2023-09-18 21:38:42 +0800

    udpate

--- plugins/init.lua
+++ plugins/init.lua
@@ -368,6 +368,14 @@ local plugins = {
             require "custom.plugins.configs.lint"
         end,
     },
+    {
+    "altermo/ultimate-autopair.nvim",
+    event={'InsertEnter','CmdlineEnter'},
+    branch='v0.6',
+    opts={
+        --Config goes here
+    },
+}
 }
 
 return plugins
