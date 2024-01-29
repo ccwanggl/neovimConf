b5084626925fd45256713409addbdc5c2f46bd17
Guoliang WANG <tswanggl@gmail.com>
2024-01-29 23:14:29 +0800

    call setup in the init file will not work must in the on_attach() when the LSP client attaches to a buffer

--- plugins/init.lua
+++ plugins/init.lua
@@ -400,9 +400,8 @@ local plugins = {
     {
         "ray-x/lsp_signature.nvim",
         event = "VeryLazy",
-        opts = {},
-        config = function(_, opts) 
-            require'lsp_signature'.setup(opts)
+        config = function()
+            require "custom.plugins.configs.lsp_signature"
         end
     }
 }
